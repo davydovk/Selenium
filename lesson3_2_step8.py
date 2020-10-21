8a527702414bab70a50efe7268abb8e51db1d6de
Давыдов Константин Юрьевич <davydov@rosenergo.com>
2020-10-21 12:18:25 +0300

    Delete unnecessary files

--- lesson3_2_step8.py
+++
@@ -1,5 +0,0 @@
-def test_input_text(expected_result, actual_result):
-    assert expected_result == actual_result, f'expected {expected_result}, got {actual_result}'
-
-
-test_input_text(11, 11)
--- test_browser_languages_chrome.py
+++
@@ -1,6 +0,0 @@
-from selenium.webdriver.chrome.options import Options
-from selenium import webdriver
-
-options = Options()
-options.add_experimental_option('prefs', {'intl.accept_languages': 'user_language'})
-browser = webdriver.Chrome(options=options)
